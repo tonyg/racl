a54859d0e39e61a4b69e46454ad67299d1967c4f
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-09-18 18:13:42 -0400

    Move example program to module+ main, for autobuilder convenience

--- examples/repl-client.rkt
+++ examples/repl-client.rkt
@@ -1,18 +1,19 @@
 #lang racket/base
 
-(require racket/cmdline)
-(require racket/tcp)
-(require racket/port)
-(require "../tcp.rkt")
+(module+ main
+  (require racket/cmdline)
+  (require racket/tcp)
+  (require racket/port)
+  (require "../tcp.rkt")
 
-(define port 2222)
+  (define port 2222)
 
-(command-line
- #:program "repl-client.rkt"
- #:once-each
- ["-p" port-number "TCP port number to connect to"
-  (set! port (string->number port-number))])
+  (command-line
+   #:program "repl-client.rkt"
+   #:once-each
+   ["-p" port-number "TCP port number to connect to"
+    (set! port (string->number port-number))])
 
-(define-values (i o) (encrypted-tcp-connect "localhost" port))
-(void (thread (lambda () (copy-port i (current-output-port)))))
-(copy-port (current-input-port) o)
+  (define-values (i o) (encrypted-tcp-connect "localhost" port))
+  (void (thread (lambda () (copy-port i (current-output-port)))))
+  (copy-port (current-input-port) o))
